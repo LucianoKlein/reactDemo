efbcfd1f8b3ba32079bfb5710605780afe2d25c9
LucianoKlein <kleinersilver@gmail.com>
2018-03-22 18:49:41 +0800

    三元表达式，在JSX里边写js表达式，以及怎么解析html

--- react2/src/components/indexBody.jsx
+++ react2/src/components/indexBody.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 export default class BodyIndex extends React.Component {
     render() {
+        let userName = '';
+        let boolInput = false;
+        //comments 
+        let html = "imooc&nbsp;lesson";
         return (
             <main>
                 <h1>这里是Body</h1>
+                <p>{userName === '' ? '用户还没有登录' : '用户名:' + userName}</p>
+                <p><input type='button' value="默认按钮" disabled={boolInput}/></p>
+                {/*我是注释*/}
+                <p>{html}</p>
+                <p dangerouslySetInnerHTML = {{__html : html}}></p>
             </main>
         )
     }
