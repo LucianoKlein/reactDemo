c77a7cd35f954d591fe01b17a9e1f24e88241c53
LucianoKlein <kleinersilver@gmail.com>
2018-03-23 08:57:34 +0800

    练习state用法

--- react2/src/components/indexBody.jsx
+++ react2/src/components/indexBody.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
 export default class BodyIndex extends React.Component {
-    componentWillMount() {
-        //定义你的逻辑就可以了
-        console.log("BodyIndex - componentWillMount");
-    }
-
-    componentDidMount() {
-        console.log("BodyIndex - componentDidMount");
+    constructor() {
+        super();
+        this.state = {
+            username: "Parry",
+            age: "18"
+        };
     }
 
     render() {
-        let userName = '';
-        let boolInput = false;
-        //comments 
-        let html = "hah&nbsp;aha";
+        setTimeout(()=>{
+            this.setState({
+                username: "Mooc",
+                age: "30"
+            });
+        }, 4000);
         return (
             <main>
-                <h1>这里是Body</h1>
-                <p>{userName === '' ? '用户还没有登录' : '用户名:' + userName}</p>
-                <p><input type='button' value="默认按钮" disabled={boolInput}/></p>
-                {/*我是注释*/}
-                <p>{html}</p>
-                <p dangerouslySetInnerHTML = {{__html : html}}></p>
+                <h1>页面的主体内容</h1>
+                <p>{this.state.username}</p>
+                <p>{this.state.age}</p>
             </main>
         )
     }
