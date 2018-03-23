ab6b44ea797c5e51b2884e3fe849827f9dab4403
LucianoKlein <kleinersilver@gmail.com>
2018-03-23 11:27:27 +0800

    react通过refs来得到DOM元素

--- react2/src/components/indexBody.jsx
+++ react2/src/components/indexBody.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import BodyChild from '../components/bodychild.jsx';
 import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
 const defaultProps = {
     username: '这是一个默认值'
 }
@@ -17,6 +18,13 @@ export default class BodyIndex extends React.Component {
         this.setState({
             age: 50
         });
+        //第一种方式
+        let mySubmitButton = document.getElementById('submitButton');
+        console.log(mySubmitButton);
+        ReactDOM.findDOMNode(mySubmitButton).style.color = "red";
+
+        //第二种方式
+        console.log(this.refs.submitButton);
     }
 
     handleChildValueChange(event) {
@@ -29,7 +37,7 @@ export default class BodyIndex extends React.Component {
                 <h1>页面的主体内容</h1>
                 <p>接收到父页面的属性 userid: {this.props.userid} username: {this.props.username} </p>
                 <p>age: {this.state.age}</p>
-                <input type="button" value="提交" onClick={this.changeUserInfo.bind(this)}/>
+                <input id="submitButton" ref="submitButton" type="button" value="提交" onClick={this.changeUserInfo.bind(this)}/>
                 <BodyChild {...this.props} handleChildValueChange={this.handleChildValueChange.bind(this)}/>
             </main>
         )
